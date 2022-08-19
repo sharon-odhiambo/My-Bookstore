132bd6f5fc553cb7e0879f57b4327200aaa53b1e
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-08-19 16:28:03 +0300

    Setup bookstore project, react router and components for page rendering

---
+++ src/components/BookContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import BookInput from './BookInput';
+import Booklist from './Booklist';
+
+const BookContainer = () => {
+  const books = [{
+    title: 'Fiesty Fiery',
+    author: 'Sharon',
+  }]
+
+  return (
+    <>
+    <Booklist books={books}/>
+    <BookInput />
+  </>
+  )
+};
+
+export default BookContainer;
---
+++ src/components/Booklist.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BookItem from './BookItem';
+
+const Booklist = ({books}) => {
+  return (
+    <ul>
+      {books.map((book) => (
+        <BookItem
+          key={book.id}
+          book={book}
+        />
+      ))}
+    </ul>
+  );
+};
+
+Booklist.propTypes = {
+  books: PropTypes.arrayOf,
+  // handleChangeProps: PropTypes.func.isRequired,
+  // deleteTodoProps: PropTypes.func.isRequired,
+};
+
+Booklist.defaultProps = {
+  books: [],
+};
+
+export default Booklist;
