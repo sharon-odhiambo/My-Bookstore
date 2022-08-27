9e39a700e9a5f7fc409c6f81db0b7477b45aebe7
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-08-27 18:32:35 +0300

    Style the bookstore to match Zeplin wireframe

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -3,25 +3,32 @@ import propTypes from 'prop-types';
 
 const BookItem = ({ book, remove, id }) => (
   <li key={id}>
-    <div className="book-details">
-      <span>{book.category}</span>
-      <span>{book.title}</span>
-      <span>{book.author}</span>
+    <div className="bookitem">
+      <div className="book-details">
+        <span>{book.category}</span>
+        <span>{book.title}</span>
+        <span>{book.author}</span>
+      </div>
+      <div className="book-buttons">
+        <button type="button">Comments</button>
+        <button type="button" id={id} onClick={remove}>Remove</button>
+        <button type="button">Edit</button>
+      </div>
     </div>
-    <div>
-      <button type="button">Comments</button>
-      <button type="button" id={id} onClick={remove}>Remove</button>
-      <button type="button">Edit</button>
-    </div>
-    <div>
-      <p>62% Completed</p>
-    </div>
-    <div>
+    <div className="progress-section">
+      <div className="circle">
+        <div className="inner-circle" />
+        <div className="inner-circle2" />
+      </div>
       <p>
-        <span>Current chapter</span>
-        <span>Chapter 3</span>
+        <span>62%</span>
+        <span>Completed</span>
       </p>
-      <button type="button">Update Progress</button>
+    </div>
+    <div className="bookchapter">
+      <span>CURRENT CHAPTER</span>
+      <span>Chapter 3</span>
+      <button type="button">UPDATE PROGRESS</button>
     </div>
   </li>
 );
